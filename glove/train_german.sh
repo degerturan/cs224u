4a9f75567b588df36c366fcae2e068b56b403df0
Catalin Voss <catalin@stanford.edu>
2016-05-24 18:31:50 -0700

    Add training scripts

---
+++ glove/train_german.sh
@@ -0,0 +1,30 @@
+CORPUS=german_ted_unprocessed.txt
+VOCAB_FILE=vocab_german_unprocessed.txt
+COOCCURRENCE_FILE=cooccurrence_german_unprocessed.bin
+COOCCURRENCE_SHUF_FILE=cooccurrence_german_unprocessed.shuf.bin
+BUILDDIR=build
+SAVE_FILE=vectors_german_unprocessed
+VERBOSE=2
+MEMORY=4.0
+VOCAB_MIN_COUNT=5
+VECTOR_SIZE=50
+MAX_ITER=15
+WINDOW_SIZE=15
+BINARY=2
+NUM_THREADS=8
+X_MAX=10
+
+$BUILDDIR/vocab_count -min-count $VOCAB_MIN_COUNT -verbose $VERBOSE < $CORPUS > $VOCAB_FILE
+if [[ $? -eq 0 ]]
+  then
+  $BUILDDIR/cooccur -memory $MEMORY -vocab-file $VOCAB_FILE -verbose $VERBOSE -window-size $WINDOW_SIZE < $CORPUS > $COOCCURRENCE_FILE
+  if [[ $? -eq 0 ]]
+  then
+    $BUILDDIR/shuffle -memory $MEMORY -verbose $VERBOSE < $COOCCURRENCE_FILE > $COOCCURRENCE_SHUF_FILE
+    if [[ $? -eq 0 ]]
+    then
+       $BUILDDIR/glove -save-file $SAVE_FILE -threads $NUM_THREADS -input-file $COOCCURRENCE_SHUF_FILE -x-max $X_MAX -iter $MAX_ITER -vector-size $VECTOR_SIZE -binary $BINARY -vocab-file $VOCAB_FILE -verbose $VERBOSE
+       
+    fi
+  fi
+fi
